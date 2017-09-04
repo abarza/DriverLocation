5ab44ce6a5962f4f2747997b1a714a8abe87aa89
Francisco Abarza <francisco@maskin.cl>
2017-09-04 18:24:46 -0300

    add listener to recyclerview's item

--- app/src/main/java/com/beetrack/test/abarza/driverlocation/location/PlaceHolder.java
+++ app/src/main/java/com/beetrack/test/abarza/driverlocation/location/PlaceHolder.java
@@ -1,18 +1,22 @@
 package com.beetrack.test.abarza.driverlocation.location;
 
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
+
 import com.beetrack.test.abarza.driverlocation.R;
 
 /**
  * Created by abarza on 04-09-17.
  */
 
-public class PlaceHolder extends RecyclerView.ViewHolder {
-
+public class PlaceHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+  private static final String TAG = PlaceHolder.class.getSimpleName();
   private TextView mPlaceTitle;
   private TextView mPlaceAddress;
+  private String mName;
 
 
   public PlaceHolder(View itemView) {
@@ -20,14 +24,16 @@ public class PlaceHolder extends RecyclerView.ViewHolder {
 
     mPlaceTitle = (TextView) itemView.findViewById(R.id.placeName);
     mPlaceAddress = (TextView) itemView.findViewById(R.id.placeAddress);
+
+    itemView.setOnClickListener(this);
   }
 
   public void updateUI(PlaceModel placeModel) {
 
-    String name = placeModel.getPlaceName();
+    mName = placeModel.getPlaceName();
     String address = placeModel.getPlaceAddress();
 
-    setLabels(name, mPlaceTitle);
+    setLabels(mName, mPlaceTitle);
     setLabels(address, mPlaceAddress);
 
 
@@ -41,5 +47,8 @@ public class PlaceHolder extends RecyclerView.ViewHolder {
     }
   }
 
-
+  @Override
+  public void onClick(View v) {
+    Toast.makeText(itemView.getContext(), mName, Toast.LENGTH_SHORT).show();
+  }
 }
